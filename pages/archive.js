ec3ba8c4e6492053f626749633a2907c001c6c04
Masamichi Kagaya <masamichi.kagaya@gmail.com>
2021-03-22 19:36:25 +0900

    Add Graphql to archive page

--- pages/archive.js
+++ pages/archive.js
@@ -1,10 +1,33 @@
+import { fetchContent } from '@/lib/contentful-hook'
 
-const Archive = () => {
+const Archive = ({ allArchives }) => {
+    console.log('allArchives:', allArchives)
     return (
         <div>
-
+            Archives
         </div>
     );
 };
 
 export default Archive;
+
+export async function getStaticProps() {
+    const res = await fetchContent(`
+        {
+            kasumibroVideoCollection {
+                items {
+                    thumbnail {
+                        url
+                    }
+                    title
+            }
+        }
+      }
+  `);
+    return {
+        props: {
+            allArchives: res.kasumibroVideoCollection.items,
+        },
+        revalidate: 1,
+    }
+}

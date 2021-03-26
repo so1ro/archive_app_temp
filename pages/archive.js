a58119cb7cb96359e90f8adc4439572e341a230b
Masamichi Kagaya <masamichi.kagaya@gmail.com>
2021-03-26 09:03:48 +0900

    Solve to make Next-image Rounded

--- pages/archive.js
+++ pages/archive.js
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import { format, parseISO } from 'date-fns'
-import { Grid, GridItem, Box, List, ListItem } from "@chakra-ui/react"
 
 import { fetchContentful } from '@/hook/contentful-hook'
 import { query_allArchives } from '@/hook/contentful-queries'
+
+import { Grid, GridItem, Box, List, ListItem } from "@chakra-ui/react"
+import { css } from '@emotion/react'
 
 const Archive = ({ allArchives }) => {
     console.log('allArchives:', allArchives)
@@ -15,13 +17,12 @@ const Archive = ({ allArchives }) => {
             <Grid templateColumns="1fr" gap={12} px={6}>
                 {allArchives.map((archive) => (
                     <Grid key={archive.sys.id} templateColumns="repeat(2, 1fr)" gap={12}>
-                        <Box borderRadius={6} overflow="hidden" >
+                        <Box overflow="hidden" css={imgBox}>
                             <Image
                                 src={archive.thumbnail.url}
                                 alt="Picture of the author"
                                 width={640}
-                                height={360}
-                            />
+                                height={360} />
                         </Box>
                         <Box>
                             <List m={0} p={0}>
@@ -45,5 +46,11 @@ export async function getStaticProps() {
         revalidate: 1,
     }
 }
+
+const imgBox = css`
+  img {
+    border-radius: 0.4rem;
+  }
+`
 
 export default Archive

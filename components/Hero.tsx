1dff71300dcc91ffc5598402e2f09d1ea92004d1
Masamichi Kagaya <masamichi.kagaya@gmail.com>
2021-05-13 09:50:31 +0900

    Clean code in Hero compo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,32 +2,32 @@ import { Box, Container } from '@chakra-ui/react';
 import Image from 'next/image'
 import HeroSnsIcons from '@/components/HeroSnsIcons';
 import HeroArchiveLink from '@/components/HeroArchiveLink';
+import { dailyNum } from '@/utils/helpers';
 
-export default function Hero({ todayImg }: { todayImg: TodayImgInterface }) {
-    todayImg.sort(function (a, b) {
-        return a.width - b.width;
-    });
-    const portraitImg = todayImg[0]
-    const landscapeImg = todayImg[1]
+export default function Hero({ allHeroImg }) {
+
+    const shapedAllImg = allHeroImg.map(pair => pair.imageCollection.items.sort((a, b) => a.width - b.width))
 
     return (
         <Box pos='relative'>
-            <Container h='100vh' zIndex={'-1'} d={{ base: 'block', lg: 'none' }}>
-                <Image src={portraitImg.url}
-                    layout="fill"
-                    objectFit="cover"
-                    quality={100}
-                    priority={true}
-                />
-            </Container>
-            <Container h='100vh' zIndex={'-1'} d={{ base: 'none', lg: 'block' }}>
-                <Image src={landscapeImg.url}
-                    layout="fill"
-                    objectFit="cover"
-                    quality={100}
-                    priority={true}
-                />
-            </Container>
+            {shapedAllImg.map((pair, i) => (
+                (i === dailyNum(shapedAllImg)) &&
+                <Box key={i}>
+                    {pair.map((img, j) => (
+                        <Container key={j}
+                            h='100vh'
+                            zIndex={'-1'}
+                            d={j === 0 ? { base: 'block', lg: 'none' } : { base: 'none', lg: 'block' }}>
+                            <Image src={img.url}
+                                layout="fill"
+                                objectFit="cover"
+                                quality={100}
+                                priority={true}
+                            />
+                        </Container>
+                    ))}
+                </Box>
+            ))}
             <HeroArchiveLink />
             <HeroSnsIcons />
         </Box>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,14 @@ import { GetStaticProps } from "next"
 
 import { fetchContentful } from "@/hook/contentful"
 import { query_allHeroImg } from "@/hook/contentful-queries"
-import { dailyNum } from '@/utils/helpers';
 
 export default function Home({ allHeroImg }: { allHeroImg: AllHeroImgInterface[] }) {
 
-  const todayImg = allHeroImg[dailyNum(allHeroImg)].imageCollection.items
   const { user, error, isLoading } = useUser();
 
   return (
     <main>
-      <Hero todayImg={todayImg} />
+      <Hero allHeroImg={allHeroImg} />
       {user && <div> Welcome {user.name}! <a href="/api/auth/logout">Logout</a> </div>}
       {isLoading && <div>Loading...</div>}
       {error && <div>{error.message}</div>}
@@ -30,6 +28,6 @@ export const getStaticProps: GetStaticProps = async () => {
     props: {
       allHeroImg: data.topHeroImgsCollection.items,
     },
-    revalidate: 1,
+    revalidate: 30,
   }
 }

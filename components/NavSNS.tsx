e5e1b7aca220f348cb14114d1d3bd5f166453611
Masamichi Kagaya <masamichi.kagaya@gmail.com>
2021-05-26 11:07:49 +0900

    Add Icon to NavSNS

--- components/NavSNS.tsx
+++ components/NavSNS.tsx
@@ -1,6 +1,7 @@
-import { Box, Link, HStack } from '@chakra-ui/react';
+import { Box, Link, HStack, Stack, useColorModeValue } from '@chakra-ui/react';
 import { useRouter } from 'next/router'
 import NextLink from 'next/link';
+import { TwitterIcon, InstagramIcon } from '@/styles/icons';
 
 export default function NavSNS({ items }: { items: TwitterCollectionItems[] }) {
 
@@ -8,20 +9,33 @@ export default function NavSNS({ items }: { items: TwitterCollectionItems[] }) {
     const { path } = router.query
     // Twitter path check!!!!
     const root = router.pathname.includes('twitter') ? 'twitter' : 'instagram'
+    const iconSize = 8
+
 
     return (
-        <HStack
-            flexWrap='wrap'
-            spacing={{ base: 4, md: 6 }}
-            justify='center'
-            px='0'
+        <Stack
+            direction={{ base: 'column', lg: 'row' }}
+            align={{ base: 'center', lg: '' }}
+            justify={{ base: '', lg: 'space-between' }}
+            w='full'
             pt={{ base: 12, lg: 16 }}
-            pb={{ base: 0, lg: 6 }}>
-            { items.map(item => (
-                (<NextLink href={`/${root}/${item.path}`} key={item.sys.id} passHref>
-                    <Link fontSize={{ base: 'sm', md: 'md' }} className={path === item.path ? 'active' : ''}>{item.name}</Link>
-                </NextLink>)
-            ))
-            }</HStack>
+            pb={{ base: 0, lg: 6 }}
+            spacing={4}>
+            {root === 'twitter' && <TwitterIcon width={iconSize} height={iconSize} color={useColorModeValue('gray.600', '#fff')} />}
+            {root === 'instagram' && <InstagramIcon width={iconSize} height={iconSize} color={useColorModeValue('gray.600', '#fff')} />}
+            <HStack
+                flexWrap='wrap'
+                spacing={{ base: 4, md: 6 }}
+                justify='center'
+                px='0'
+                alignSelf={{ base: 'center', lg: 'flex-end' }}
+            >
+                {items.map(item => (
+                    (<NextLink href={`/${root}/${item.path}`} key={item.sys.id} passHref>
+                        <Link fontSize={{ base: 'sm', md: 'md' }} className={path === item.path ? 'active' : ''}>{item.name}</Link>
+                    </NextLink>)
+                ))
+                }</HStack>
+        </Stack>
     )
 }

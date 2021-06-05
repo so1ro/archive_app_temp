b39b817b097e7dc56aed5b3bd9bda5fed7d4df24
Masamichi Kagaya <masamichi.kagaya@gmail.com>
2021-06-05 18:28:59 +0900

    Adjust Center without Window Hook with Flex

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import { Text, VStack, Center, Spinner, useColorModeValue } from '@chakra-ui/react';
-import useWindowSize from '@/utils/useWindowSize';
+import { Text, VStack, Center, Spinner, useColorModeValue, Flex } from '@chakra-ui/react';
 import { highlight_color } from '@/styles/colorModeValue';
 
 export default function LodingSpinner() {
-    const { height: innerHeight } = useWindowSize();
-
     return (
-        <Center h={`${innerHeight}px`}>
-            <VStack spacing={4}>
-                <Spinner thickness="4px"
-                    speed="0.65s"
-                    emptyColor="gray.200"
-                    color={useColorModeValue(highlight_color.l, highlight_color.d)}
-                    size="xl" />
-                <Text fontSize='sm'>このまま少々お待ち下さい...</Text>
-            </VStack>
-        </Center>
+        <Flex flexGrow={1} direction='row'>
+            <Center w='full'>
+                <VStack spacing={4}>
+                    <Spinner thickness="4px"
+                        speed="0.65s"
+                        emptyColor="gray.200"
+                        color={useColorModeValue(highlight_color.l, highlight_color.d)}
+                        size="xl" />
+                    <Text fontSize='sm'>このまま少々お待ち下さい...</Text>
+                </VStack>
+            </Center>
+        </Flex>
     );
 }

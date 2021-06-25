bb72b4292e28f107712b7bc292a1c8873e2cf3cd
Masamichi Kagaya <masamichi.kagaya@gmail.com>
2021-06-25 16:45:15 +0900

    Restart PWA

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
 // with SW (Service Worker)
-// const withPWA = require('next-pwa')
+const withPWA = require('next-pwa')
 
-// module.exports = withPWA({
-//     images: {
-//         domains: ['images.ctfassets.net', 'pbs.twimg.com', 'abs.twimg.com'],
-//     },
-//     pwa: {
-//         dest: 'public'
-//     },
-// })
+module.exports = withPWA({
+    images: {
+        domains: ['images.ctfassets.net', 'pbs.twimg.com', 'abs.twimg.com'],
+    },
+    pwa: {
+        dest: 'public'
+    },
+})
 
 // Develop without SW (Service Worker)
-module.exports = {
-    images: {
-        domains: ['images.ctfassets.net', 'pbs.twimg.com', 'abs.twimg.com', 'i.ytimg.com'],
-    },
-}
+// module.exports = {
+//     images: {
+//         domains: ['images.ctfassets.net', 'pbs.twimg.com', 'abs.twimg.com', 'i.ytimg.com'],
+//     },
+// }
 
 // Twitter : 'pbs.twimg.com'
 // Contentful : 'images.ctfassets.net'

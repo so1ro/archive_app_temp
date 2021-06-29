6b9a093adf8de2bcb370d711bc7bbd4c2ecfb380
Masamichi Kagaya <masamichi.kagaya@gmail.com>
2021-06-29 10:17:00 +0900

    Remove NEXT_PUBLIC_ from TWITTER_API_KEY

--- lib/twitter.ts
+++ lib/twitter.ts
@@ -1,7 +1,7 @@
 export const getTweets = async (id) => {
     const res = await fetch(
         `https://api.twitter.com/2/users/${id}/tweets?max_results=16&exclude=replies,retweets`,
-        { headers: { Authorization: `Bearer ${process.env.NEXT_PUBLIC_TWITTER_API_KEY}` } }
+        { headers: { Authorization: `Bearer ${process.env.TWITTER_API_KEY}` } }
     )
 
     const tweets = await res.json()

542fe826cdf25d005fdf05c732e92f53a2b76e28
Masamichi Kagaya <masamichi.kagaya@gmail.com>
2021-06-30 15:23:16 +0900

    Remove NEXT_PUBLIC_ from CONTENTFUL

--- hook/contentful.js
+++ hook/contentful.js
@@ -1,5 +1,5 @@
-const space = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID
-const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
+const space = process.env.CONTENTFUL_SPACE_ID
+const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN
 
 export async function fetchContentful(query) {
     // add a try / catch loop for nicer error handling

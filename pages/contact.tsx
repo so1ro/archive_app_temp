7bdb549ba9e8a9f00e483200962912c1e65a3384
Masamichi Kagaya <masamichi.kagaya@gmail.com>
2021-07-01 10:31:31 +0900

    Change Regex to email

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -22,9 +22,12 @@ const Contact = () => {
   const toast = useToast()
 
   const validate = (input) => {
+    // Ref : https://emailregex.com/
+    const regexEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i
+
     if (input.target.name === 'email') {
       if (input.target.value === '') { return setError({ error: { email: '' } }) }
-      if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i.test(input.target.value)) {
+      if (!regexEmail.test(input.target.value)) {
         setError({ error: { email: 'Emailアドレスをご入力ください。' } })
       } else {
         setError({ error: { email: '' } })

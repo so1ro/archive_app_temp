3b35af4886ee176c69e471f7f695342175903f40
Masamichi Kagaya <masamichi.kagaya@gmail.com>
2021-07-02 18:18:23 +0900

    Modify icon path in _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -18,27 +18,27 @@ class MyDocument extends Document {
                     <meta name='description' content="Author's Archive app" />
                     <meta name='format-detection' content='telephone=no' />
                     <meta name='mobile-web-app-capable' content='yes' />
-                    <meta name='msapplication-config' content='/public/icons/browserconfig.xml' />
+                    <meta name='msapplication-config' content='/icons/browserconfig.xml' />
                     <meta name='msapplication-TileColor' content='#da532c' />
                     <meta name='msapplication-tap-highlight' content='no' />
                     <meta name='theme-color' content='#FFFFFF' />
 
-                    <link rel="apple-touch-icon" href="public/icons/apple-touch-icon.png" />
-                    <link rel="apple-touch-icon" sizes="57x57" href="public/icons/apple-touch-icon-57x57.png" />
-                    <link rel="apple-touch-icon" sizes="72x72" href="public/icons/apple-touch-icon-72x72.png" />
-                    <link rel="apple-touch-icon" sizes="76x76" href="public/icons/apple-touch-icon-76x76.png" />
-                    <link rel="apple-touch-icon" sizes="114x114" href="public/icons/apple-touch-icon-114x114.png" />
-                    <link rel="apple-touch-icon" sizes="120x120" href="public/icons/apple-touch-icon-120x120.png" />
-                    <link rel="apple-touch-icon" sizes="144x144" href="public/icons/apple-touch-icon-144x144.png" />
-                    <link rel="apple-touch-icon" sizes="152x152" href="public/icons/apple-touch-icon-152x152.png" />
-                    <link rel="apple-touch-icon" sizes="180x180" href="public/icons/apple-touch-icon-180x180.png" />
-                    <link rel="apple-touch-icon" sizes="192x192" href="public/icons/icon-192x192.png" />
+                    <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+                    <link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-touch-icon-57x57.png" />
+                    <link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-touch-icon-72x72.png" />
+                    <link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-touch-icon-76x76.png" />
+                    <link rel="apple-touch-icon" sizes="114x114" href="/icons/apple-touch-icon-114x114.png" />
+                    <link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-touch-icon-120x120.png" />
+                    <link rel="apple-touch-icon" sizes="144x144" href="/icons/apple-touch-icon-144x144.png" />
+                    <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-touch-icon-152x152.png" />
+                    <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon-180x180.png" />
+                    <link rel="apple-touch-icon" sizes="192x192" href="/icons/icon-192x192.png" />
 
                     <link rel='shortcut icon' href='/favicon.ico' />
-                    <link rel='icon' type='image/png' sizes='32x32' href='/public/icons/favicon-32x32.png' />
-                    <link rel='icon' type='image/png' sizes='16x16' href='/public/icons/favicon-16x16.png' />
+                    <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
+                    <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
                     <link rel='manifest' href='/manifest.json' />
-                    <link rel='mask-icon' href='/public/icons/safari-pinned-tab.svg' color='#bf5555' />
+                    <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#bf5555' />
 
                     {/* <meta name='twitter:card' content='summary' />
                     <meta name='twitter:url' content='https://yourdomain.com' />
